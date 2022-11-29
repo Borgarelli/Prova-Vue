ccbc3adcdfeccd49cb74e5b837e944eda58ebc58
Borgarelli <borgarellitavares@outlook.com>
2022-11-29 13:09:43 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import AppView from '../views/AppView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'app',
+    component: AppView,
+    children: [
+      {
+        path: '/usuario',
+        component: () => import('../views/UsuarioView.vue')
+      },
+      {
+        path: '/',
+        component: () => import('../views/HomeView.vue')
+      },
+      {
+        path: '/about',
+        component: () => import('../views/AboutView.vue')
+      },
+      {
+        path: '/teste',
+        component: () => import('../views/TesteView.vue')
+      },
+      {
+        path: '/anotacao',
+        component: () => import('../views/AnotacaoView.vue')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/AppView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="appView">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/usuario">Usuário</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/teste">Teste</router-link> |
+      <router-link to="/anotacao">Anotacao</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
